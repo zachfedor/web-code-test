d31a0e36c65cbd330f4268662aa9772380d9c0ea
Michael Bunting <mbunting@webstaurantstore.com>
2020-12-02 12:05:40 -0500

    Adding tailwind and basic skeleton of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10 m-auto bg-blue-50 min-h-screen">
+      <div className="border border-gray-300 rounded-lg w-full bg-white p-10 shadow-lg">
+        <h1 className="m-0 p-0 text-2xl font-semibold">Restaurant Order System</h1>
+        <hr className="my-3 border border-0 border-t-1 border-gray-200"></hr>
+        Select products below to add to the ordering guide
+      </div>
     </div>
   );
 }
