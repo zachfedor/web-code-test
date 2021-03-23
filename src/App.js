36d211173d2ac847105bcaf903f1d083c1d0e828
Zach Fedor <zachfedor@gmail.com>
2021-03-23 12:44:59 -0400

    Feature: calculate sub-totals and total of BuyList
    - Refactor buylist array into map for more performant lookups using the
      product's itemNumber. Values will hold entire product object with
      added quantity.
    - Reduce the buylist into a single sum to calculate the entire total.
    - Create function to build onChange handler for each product's quantity
      input with verification of number type and minimum of 1 unit to avoid
      math errors at runtime.
    - Refactor BuyList component to iterate over product array now coming from
      props to simplify its logic and remove StockProductData.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
 import * as React from 'react';
 import './App.css';
-import StockProducts from './StockProducts.jsx';
 import BuyList from './BuyList';
+import StockProducts from './StockProducts.jsx';
+import StockProductsData from "./data/stockProducts.json";
+
+// Convert product data into Map for faster/easier lookups
+const StockProductsMap = new Map(StockProductsData.map((product) => [product.itemNumber, product]));
 
 function App() {
-  const [buyListItems, setBuyListItems] = React.useState([]);
+  // Map containing products added to BuyList
+  const [buyListProducts, setBuyListProducts] = React.useState(new Map());
+
+  // Sum of all BuyList product sub-totals (i.e. price by quantity)
+  const buyListTotal = Array.from(buyListProducts.values()).reduce((total, product) => {
+    return total + (product.price * product.quantity);
+  }, 0);
 
   /**
    * Create event handler to add product to BuyList for a given itemNumber
    *
-   * When the event handler runs, the product will be added to BuyList array with a default
+   * When the event handler runs, the product will be added to BuyList map with a default
    * quanity of 1, if it isn't already included. If the product has already been added to the
    * BuyList, nothing happens.
    * @param {string} itemNumber - The unique id of a product
-   * @returns {function} - An event handler
+   * @returns {function} - An event handler for product's onClick method
    */
   const addToBuyListHandler = (itemNumber) => () => {
-    if (!buyListItems.find(item => item.itemNumber === itemNumber)) {
-      setBuyListItems([...buyListItems, { itemNumber, quantity: 1 }]);
+    if (!buyListProducts.has(itemNumber)) {
+      buyListProducts.set(itemNumber, { ...StockProductsMap.get(itemNumber), quantity: 1 });
+      setBuyListProducts(new Map(buyListProducts));
+    }
+  };
+
+  /**
+   * Create event handler to update product quantity in BuyList for a given itemNumber
+   *
+   * When the event handler runs, the product's quantity will be set to the event target's value.
+   * @param {string} itemNumber - The unique id of a product
+   * @returns {function} - An event handler for the product's quantity input onChange method
+   */
+  const buyListQuantityHandler = (itemNumber) => (event) => {
+    const quantity = parseInt(event.target.value, 10);
+    if (typeof quantity === 'number' && quantity > 0) {
+      buyListProducts.set(itemNumber, { ...buyListProducts.get(itemNumber), quantity });
+      setBuyListProducts(new Map(buyListProducts));
     }
   };
 
   /**
    * Create event handler to remove product from BuyList for a given itemNumber
    *
-   * When the event handler runs, the product will be filtered out of the BuyList array.
+   * When the event handler runs, the product will be removed from the BuyList.
    * @param {string} itemNumber - The unique id of a product
+   * @returns {function} - An event handler for product's remove button onClick method
    */
   const removeFromBuyListHandler = (itemNumber) => () => {
-    setBuyListItems(buyListItems.filter(item => item.itemNumber !== itemNumber));
+    buyListProducts.delete(itemNumber)
+    setBuyListProducts(new Map(buyListProducts));
   };
 
   return (
@@ -40,11 +68,15 @@ function App() {
 
         <StockProducts addToBuyListHandler={addToBuyListHandler} />
 
-        <BuyList items={buyListItems} removeFromBuyListHandler={removeFromBuyListHandler} />
+        <BuyList
+          buyListQuantityHandler={buyListQuantityHandler}
+          products={Array.from(buyListProducts.values())}
+          removeFromBuyListHandler={removeFromBuyListHandler}
+        />
 
         <div className="text-right font-semibold text-lg mt-4">
           Total:
-          <span className="text-xl ml-2">$0.00</span>
+          <span className="text-xl ml-2">{`$ ${buyListTotal}`}</span>
         </div>
       </div>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 import StockProductsData from './data/stockProducts.json';
 
-test('renders learn react link', () => {
+test('renders restuarant order system', () => {
   render(<App />);
   const linkElement = screen.getByText(/Restaurant Order System/i);
   expect(linkElement).toBeInTheDocument();
@@ -45,4 +45,39 @@ test('can remove product from BuyList', () => {
   userEvent.click(button);
   expect(button).not.toBeInTheDocument();
   expect(screen.getAllByText(testProduct.name).length).toBe(1);
+});
+
+test('can recalculate BuyList total when quantity changed or item removed', () => {
+  const productA = StockProductsData[0];
+  const productB = StockProductsData[1];
+
+  // Render the app and add the test product to BuyList
+  render(<App />);
+  userEvent.click(screen.getByRole('button', { name: productA.name }));
+
+  // Product prices can be shown in up to 3 places depending on items and quantities in BuyList:
+  // 1. The per unit price if product is added to BuyList
+  // 2. That product's sub-total if its quantity is 1
+  // 3. The BuyList total if its quantity is 1 and there are no other products
+  expect(screen.getAllByText(`$ ${productA.price}`).length).toBe(3);
+
+  // Add product B and verify change in total
+  userEvent.click(screen.getByRole('button', { name: productB.name }));
+  expect(screen.getAllByText(`$ ${productA.price}`).length).toBe(2); // per unit and sub-total
+  expect(screen.getAllByText(`$ ${productB.price}`).length).toBe(2); // per unit and sub-total
+  expect(screen.getAllByText(`$ ${productA.price + productB.price}`).length).toBe(1); // total
+
+  // Increase quantity of a product A to ten units
+  const productAQty = screen.getAllByRole('spinbutton')[0];
+  userEvent.type(productAQty, '0'); // append a '0' after the default of '1'
+
+  // Verify change in total
+  expect(screen.getByText(`$ ${(productA.price * 10) + productB.price}`)).toBeInTheDocument();
+
+  // Remove product A
+  userEvent.click(screen.getAllByRole('button', { name: 'Remove' })[0]);
+
+  // Verify change in total
+  expect(screen.getAllByText(`$ ${productB.price}`).length).toBe(3); // per unit, sub-total, total
+  expect(screen.queryByText(`$ ${productA.price}`)).not.toBeInTheDocument();
 });
